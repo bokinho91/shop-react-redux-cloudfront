080bd9904b3837c49e0da2a8e817448f8898133a
bokinho91 <boris.vujakovic@gmail.com>
2025-10-15 10:38:22 +0200

    Add Basic Auth to import request

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -35,6 +35,9 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       params: {
         name: encodeURIComponent(file?.name),
       },
+      headers: {
+        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+      },
     });
     console.log("Presigned URL: ", response.data);
     console.log("File to upload: ", file?.name);
